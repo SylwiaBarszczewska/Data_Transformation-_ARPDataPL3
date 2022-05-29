c3bf581c919caa811deb635c3487d46beffad880
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-29 10:03:18 +0200

    iloc and loc

--- part1.py
+++ part1.py
@@ -10,20 +10,26 @@ s1 = pd.Series(data=list_values, index=['A', 'B', 'C'], dtype=int)
 
 # Tworzenie DataFrame
 data1 = {
-    "col1": [1, 2, 3],
-    "col2": ['A', 'B', 'C'],
-    "col3": [1.1, 2.2, 3.3]
+    "col1": [1, 2, 3, 4, 5, 6],
+    "col2": ['A', 'B', 'C', 'Unknown', 'D', 'E'],
+    "col3": [1.1, 2.2, 3.3, 0, 1, 2]
 }
 
-numpy_data = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
-df1 = pd.DataFrame(data=data1, index=['Aa', 'Bb', 'Cc'])
-df2 = pd.DataFrame(data=numpy_data, index=['Aa', 'Bb', 'Cc'], columns=['col1', 'col2', 'col3'])
+# numpy_data = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+df1 = pd.DataFrame(data=data1, index=['Aa', 'Bb', 'Cc', 'Dd', 'Ee', 'Ff'])
+# df2 = pd.DataFrame(data=numpy_data, index=['Aa', 'Bb', 'Cc'], columns=['col1', 'col2', 'col3'])
 print(df1)
 
 # Wyswietlanie danych
-print(df1.shape) # zwraca wymiar tabeli
+# print(df1.shape)  # zwraca wymiar tabeli
+# print(df1.columns)
+# print(df1.index)
 
-#operacje matematyczne
+sample_data_iloc = df1.iloc[0]
+sample_data_loc = df1.loc['Aa']
+print(sample_data_loc)
+
+# operacje matematyczne
 
 # Dodawanie kolumn
 
