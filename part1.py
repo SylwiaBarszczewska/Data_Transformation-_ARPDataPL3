0489634bcc3871d4289adb561f08533da08ad551
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-29 09:33:00 +0200

    added series

--- part1.py
+++ part1.py
@@ -1,6 +1,12 @@
 # import
+import pandas as pd
+import numpy as np
 
 # Tworzenie Series
+list_values = [1, 2, 3]
+array_values = np.array([1, 2.3, 3])
+s1 = pd.Series(data=list_values, index=['A', 'B', 'C'], dtype=int)
+print(s1)
 
 # Tworzenie DataFrame
 
