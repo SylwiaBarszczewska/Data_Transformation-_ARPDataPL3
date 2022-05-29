41daad5c3a3e8f0b58d8f8a275ea4ac6fa68d200
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-29 15:20:34 +0200

    head, tail, sample

--- part2.py
+++ part2.py
@@ -8,8 +8,11 @@ data = pd.read_csv('Datasets/titanic.csv', sep=';')
 # print(data.info())
 # print(data.describe())  # opis dla numerycznych kolumns
 categorical_data = data.select_dtypes(object)
-print(categorical_data.describe())
+# print(categorical_data.describe())
 
+print(data.head(n=10))  # pierwsze 10 rekordow
+print(data.tail(n=10))  # ostatnie 10 rekordow
+print(data.sample(n=10))  # randomowa probka danych - 10 rekordow
 
 # usuwanie danych
 
