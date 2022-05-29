d0a06dcc59d90c58783bd19e20658476287e79ab
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-29 13:19:47 +0200

    Added file part2

---
+++ part2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# odczytywanie danych
+
+# usuwanie danych
+
+# Usuwanie nan
+
+# standaryzacja danych
+
+# duplikaty
+
+# dyskretyzacja
+
+# one hot encoding
+
+# usuwanie szumów - outliers
+
+# praca z datami
+
+# normalizacja, standaryzacja
+
+# transformacja logarytmiczna
