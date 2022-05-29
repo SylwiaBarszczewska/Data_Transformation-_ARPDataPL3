edf105468a9d95ff48e1c24ebb72af96d08987cf
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-29 09:43:44 +0200

    added dataframe2

--- part1.py
+++ part1.py
@@ -16,9 +16,10 @@ data1 = {
 }
 
 numpy_data = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
-columns=['New_col1', 'New_col2', 'New_col3']
-df = pd.DataFrame(data=data1, index=['Aa', 'Bb', 'Cc'])
-print(df)
+columns = ['col1', 'col2', 'col3']
+df1 = pd.DataFrame(data=data1, index=['Aa', 'Bb', 'Cc'])
+df2 = pd.DataFrame(data=numpy_data, index=['Aa', 'Bb', 'Cc'], columns=columns)
+print(df2)
 
 
 # Wyswietlanie danych
