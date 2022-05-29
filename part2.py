d239b6afe73d4e5939ecf11a14fe11d3063954a8
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-29 15:17:31 +0200

    read data

--- part2.py
+++ part2.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # odczytywanie danych
+data = pd.read_csv('Datasets/titanic.csv', sep=';')
+# print(data)
+# print(data.info())
+# print(data.describe())  # opis dla numerycznych kolumns
+categorical_data = data.select_dtypes(object)
+print(categorical_data.describe())
+
 
 # usuwanie danych
 
